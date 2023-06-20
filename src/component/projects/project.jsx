dea44be0c5eef3bf6893285e1cfa56abd6ba688e
Ahemd <ahmedsaye23156@gmail.com>
2023-06-20 21:13:31 +0300

    add projects

--- src/component/projects/project.jsx
+++ src/component/projects/project.jsx
@@ -6,9 +6,9 @@ let Project = () => {
             <div id='Projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16' >
                 <h1 className='text-4xl font-bold text-center text-[#001b5e]'>projects</h1>
                 <p className='text-center py-8'>
-                    The challenges I faced while creating my project pushed me to think critically and seek creative solutions. <br /> </p>
+                I have successfully completed several full-stack projects utilizing React, Next.js, and Firebase. These projects showcase my proficiency in building robust and scalable web applications. By leveraging the power of React, I have developed intuitive user interfaces that offer seamless user experiences. Next.js has allowed me to create dynamic and server-side rendered applications, optimizing performance and SEO. Additionally, I have integrated Firebase as a back-end solution, utilizing its real-time database, authentication, and hosting features. These projects demonstrate my ability to handle end-to-end development, ensuring secure data management and efficient deployment. </p>
                 
-                <div className='grid sm:grid-cols-3 gap-10'>
+                <div className='grid sm:grid-cols-2 gap-12'>
                     <Data/>
                 </div>
          </div>
--- src/component/projects/src.jsx
+++ src/component/projects/src.jsx
@@ -3,29 +3,41 @@ let Data = () => {
         {
             src: "./Work1.png",
            name: "Spotify Clone ",
+           tech: "React Js & Mui",
+           gitHub:"/"
         },
         {
-            src: "./Work1.png",
-           name: "Spotify Clone ",
+            src: "./work2.png",
+            name: "YpuTube Clone ",
+            tech: "ReactJs & Mui",
+            gitHub:"/"
+           
         },
         {
-            src: "./Work1.png",
-           name: "Spotify Clone ",
+            src: "./work3.png",
+            name: "Gym App",
+            tech: "ReactJs & Mui",
+            gitHub:"/"
         },
         {
-            src: "./Work1.png",
-           name: "Spotify Clone ",
+            src: "./work4.png",
+            name: "DashBoard ",
+            tech: "ReactJs & Mui",
+            gitHub:"/"
         },
     ]
     let item = items.map((e) => {
         return (
             <>
-                <div>
-                <img src={ e.src } alt="/" />
-                <div>
-                        <h3 style={ {
-                       marginTop:"10px" 
-                    }}>{ e.name}</h3>
+                <div className="relative flex items-center justify-center h-auto w-full  group  hover:bg-gradient-to-r from-gray-200 to-[#001b5e]"
+                   >
+                <img  className="rounded-xl group-hover:opacity-10" src={ e.src } alt="/" />
+                <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                        <h3 className="text-2xl font-bold trtacking-wider text-center text-white " >{ e.name }</h3>
+                        <p className="pb-4 font-bold pt-2 text-white text-center ">{ e.tech}</p>
+                        <a href={e.gitHub}>
+                            <p className="text-center p-3 rounded-ld bg-white text-gary-700 font-bold  cursor-pointer text-lg rounded-lg">More Info</p>
+                        </a>
                 </div>
                 </div>
             </>
