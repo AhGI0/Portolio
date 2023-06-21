81cd9f56da2824b8c975b8da51f543123a83a984
Ahemd <ahmedsaye23156@gmail.com>
2023-06-21 11:27:18 +0300

    nw

--- src/component/projects/project.jsx
+++ src/component/projects/project.jsx
@@ -8,7 +8,7 @@ let Project = () => {
                 <p className='text-center py-8'>
                 I have successfully completed several full-stack projects utilizing React, Next.js, and Firebase. These projects showcase my proficiency in building robust and scalable web applications. By leveraging the power of React, I have developed intuitive user interfaces that offer seamless user experiences. Next.js has allowed me to create dynamic and server-side rendered applications, optimizing performance and SEO. Additionally, I have integrated Firebase as a back-end solution, utilizing its real-time database, authentication, and hosting features. These projects demonstrate my ability to handle end-to-end development, ensuring secure data management and efficient deployment. </p>
                 
-                <div className='grid sm:grid-cols-2 gap-12'>
+                <div className='grid sm:grid-cols-2 gap-10'>
                     <Data/>
                 </div>
          </div>
--- src/component/projects/src.jsx
+++ src/component/projects/src.jsx
@@ -32,7 +32,7 @@ let Data = () => {
             <>
                 <div className="relative flex items-center justify-center h-auto w-full  group  hover:bg-gradient-to-r from-gray-200 to-[#001b5e]"
                    >
-                <img  className="rounded-xl group-hover:opacity-10" src={ e.src } alt="/" />
+                <img  className="rounded-xl group-hover:opacity-10 shadow-2xl" src={ e.src } alt="/" />
                 <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
                         <h3 className="text-2xl font-bold trtacking-wider text-center text-white " >{ e.name }</h3>
                         <p className="pb-4 font-bold pt-2 text-white text-center ">{ e.tech}</p>
